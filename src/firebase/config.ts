c61d82236288596f0fbe2ff0775c3886961e38b4
hussein awaesha <h.awaesha97@gmail.com>
2025-11-03 11:55:57 +0000

    this is the firebase configs from the firebase console
    // For Firebase

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,6 +6,14 @@
 // From the Firebase SDK snippet, copy the config object and paste it here.
 //
 // Note: This is a public, client-side configuration object.
-const firebaseConfig = undefined;
+const firebaseConfig = {
+  apiKey: "AIzaSyD0ML2oJGXdSejsuouoGdKil9gvYx1e4NQ",
+  authDomain: "deliverzler-restaurant.firebaseapp.com",
+  projectId: "deliverzler-restaurant",
+  storageBucket: "deliverzler-restaurant.appspot.com",
+  messagingSenderId: "557624783289",
+  appId: "1:557624783289:web:ad4d2d11917978068a8668",
+  measurementId: "G-TCZWFX0R22"
+};
 
 export {firebaseConfig};
