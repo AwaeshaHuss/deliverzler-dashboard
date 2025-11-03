5e6cc89fadc6a47503b0c0c3781d76d3a48e5510
hussein awaesha <h.awaesha97@gmail.com>
2025-11-03 11:53:36 +0000

    please be faster, you are so slow

---
+++ src/firebase/config.ts
@@ -0,0 +1,11 @@
+// To find your Firebase config object, go to the Firebase console, select
+// your project, and go to Project settings.
+//
+// In the "Your apps" card, select the app for which you need a config object.
+//
+// From the Firebase SDK snippet, copy the config object and paste it here.
+//
+// Note: This is a public, client-side configuration object.
+const firebaseConfig = undefined;
+
+export {firebaseConfig};
